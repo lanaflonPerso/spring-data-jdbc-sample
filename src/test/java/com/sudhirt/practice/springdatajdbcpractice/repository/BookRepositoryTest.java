6bc2a63b83ba86fcdbbd79fc0837300e2ee069c8
Sudhir Tumati <mailofsudhir@gmail.com>
2019-10-27 09:25:29 +0800

    fix: add test case to verify book with multiple authors scenario

--- src/test/java/com/sudhirt/practice/springdatajdbcpractice/repository/BookRepositoryTest.java
+++ src/test/java/com/sudhirt/practice/springdatajdbcpractice/repository/BookRepositoryTest.java
@@ -22,18 +22,27 @@ public class BookRepositoryTest {
 		Author author = Author.builder().firstName("John").lastName("Miller").dateOfBirth(LocalDate.of(1972, 03, 01))
 				.build();
 		authorRepository.save(author);
-
 		Book book = Book.builder().name("My Book").isbn("ISBN1234").publishedDate(LocalDate.of(2018, 12, 12))
 				.price(12.99).build();
 		book.addAuthor(authorRepository.save(author));
 		bookRepository.save(book);
+		Book savedBook = bookRepository.findById(book.getId()).orElseThrow(RuntimeException::new);
+		assertThat(savedBook.getAuthorRefs()).size().isEqualTo(1);
+	}
 
-		assertThat(book.getId()).isNotNull();
-
-		// Book savedBook =
-		// bookRepository.findById(book.getId()).orElseThrow(RuntimeException::new);
-		bookRepository.findAll();
-		// assertThat(savedBook.getAuthorRefList()).size().isEqualTo(1);
+	@Test
+	public void createBookWithMultipleAuthors() {
+		Author author1 = Author.builder().firstName("John").lastName("Miller").dateOfBirth(LocalDate.of(1972, 03, 01))
+				.build();
+		Author author2 = Author.builder().firstName("John").lastName("Miller").dateOfBirth(LocalDate.of(1972, 03, 01))
+				.build();
+		Book book = Book.builder().name("My Book").isbn("ISBN1234").publishedDate(LocalDate.of(2018, 12, 12))
+				.price(12.99).build();
+		book.addAuthor(authorRepository.save(author1));
+		book.addAuthor(authorRepository.save(author2));
+		bookRepository.save(book);
+		Book savedBook = bookRepository.findById(book.getId()).orElseThrow(RuntimeException::new);
+		assertThat(savedBook.getAuthorRefs()).size().isEqualTo(2);
 	}
 
 }
